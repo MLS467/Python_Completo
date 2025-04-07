21f6e210f9af852f7045186824531523ac4f92f0
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-04-07 17:18:32 -0300

    Seção 6: Módulos Python - OS, datatime, sys, json, csv, selenium, pillow e mais

--- aula193/rascunho.py
+++ aula193/rascunho.py
@@ -1,7 +1,12 @@
 from pathlib import Path
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-import time
+from selenium.webdriver.common.by import By
+
+# from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 ROOT_FOLDER = Path(__file__).parent
 CHROMEDRIVE_EXEC = ROOT_FOLDER / "drivers" / "chromedriver.exe"
@@ -16,6 +21,39 @@ chrome_service = Service(executable_path=CHROMEDRIVE_EXEC)
 params = {"service": chrome_service, "options": chrome_options}
 chrome_browser = webdriver.Chrome(**params)
 
+if __name__ == "__main__":
+    # tempo de espera antes de fechar
+    TIME_TO_WAIT = 120
 
-chrome_browser.get("https://www.google.com.br/")
-time.sleep(30)
+    # abrindo a página
+    chrome_browser.get("http://localhost:8000/html/")
+
+    # esperando até encontrar o elemento selecionado
+    search_input = WebDriverWait(chrome_browser, TIME_TO_WAIT).until(
+        EC.presence_of_element_located((By.ID, "email"))
+    )
+
+    # search_input.send_keys("Hello Word test")
+    for i in range(2):
+        # pegando o elemento pai para pegar as tags filhas
+        classe = "main-header-form"
+        element_father = chrome_browser.find_element(By.CLASS_NAME, classe)
+
+        # pegando vários elementos por tags find_elements
+        inputs = element_father.find_elements(By.TAG_NAME, "input")
+
+        # pegando um campo com find_element
+        text_area = element_father.find_element(By.TAG_NAME, "textarea")
+
+        # escrevendo no campo send_keys
+        inputs[0].send_keys("John")
+        inputs[1].send_keys("Doe")
+        inputs[2].send_keys("John.Doe@gmail.com")
+        text_area.send_keys("aaaaaaaaaaaaaaassssssssssssssddddddddddddd")
+
+        # se sabe se o elemento vai estar pronto é só usar o find_element
+        btn_send = chrome_browser.find_element(By.ID, "send_menssage")
+        time.sleep(10)
+        btn_send.click()
+
+time.sleep(TIME_TO_WAIT)
