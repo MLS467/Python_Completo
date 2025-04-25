fc2a5303bdeb9a549c360ad35b26963bc382050b
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-04-25 15:58:06 -0300

    aula_12 -> usando_o_context_para_enviar_dados_para_dentro_dos_templates_do_django

--- django_aula/home/views.py
+++ django_aula/home/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "home/home.html")
+    contexto = {
+        "title_text": "Home Bolada",
+        "description_text": "Essa é a página inicial do site",
+    }
+
+    return render(request, "home/home.html", contexto)
