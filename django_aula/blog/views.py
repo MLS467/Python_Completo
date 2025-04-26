e7b4f75f71be1bfbbbbf9a0b58c4756b663724e9
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-04-26 02:21:30 -0300

    aula_18 -> criando_os_dados_de_posts_(data.py)_e_usando_o_loop_for_no_template_django

--- django_aula/blog/views.py
+++ django_aula/blog/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
+# from base.data import posts
+import requests
+
 
 # Create your views here.
 def blog(request):
+
+    result_request = requests.get("https://jsonplaceholder.typicode.com/posts")
+
     context_blog = {
         "title_aba": "Blog",
         "title": "Blog",
         "content": "Welcome to the blog page!",
+        "posts": result_request.json(),
     }
     return render(request, "blog/home.html", context=context_blog)
 
