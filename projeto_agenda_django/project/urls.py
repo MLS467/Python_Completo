79e0fe30f242074d67a6e800e5b40606c41cae1c
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-04-27 18:05:44 -0300

    aula_2 -> pasta_base_de_templates_e_arquivos_estáticos_(base_templates_base_static) 2025-04-27 18:05:36

--- projeto_agenda_django/project/urls.py
+++ projeto_agenda_django/project/urls.py
@@ -21,5 +21,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("contact/", include(contact_urls)),
+    path("", include(contact_urls)),
 ]
