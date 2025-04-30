ea6d05844337c87c15473a6f7e3f4aa558bd374a
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-04-30 01:27:43 -0300

    aula_7 -> customizando_as_opções_admin.modeladmin_(área_administrativa_do_django) 2025-04-30 01:27:36

--- projeto_agenda_django/contact/admin.py
+++ projeto_agenda_django/contact/admin.py
@@ -6,4 +6,37 @@ from contact.models import Contact
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "phone",
+        "email",
+        "description",
+        "created_date",
+    )
+
+    # filtra os dados na área admin
+    # list_filter = ("created_date",)
+
+    # adiciona um campo de pesquisa pela coluna desejada
+    search_fields = ("id",)
+
+    # ordena em modo descrecente (-), creceste é padrão
+    ordering = ("-id",)
+
+    # faz uma listagem de (n) contatos para não carregar muitos de uma vez só
+    list_per_page = 10
+
+    # limita a listagem em 100 registros
+    list_max_show_all = 100
+
+    # adiciona um campo editável na listagem
+    list_editable = (
+        "last_name",
+        "email",
+    )
+
+    # adiciona um link para editar o contato na listagem
+    # mas não pode ser editável com list_editable
+    list_display_links = ("id", "first_name")
