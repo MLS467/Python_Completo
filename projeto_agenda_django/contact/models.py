6e5e8e89e09a32f23fa3c5c92467c67b75be33ed
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-04-29 23:34:40 -0300

    aula_5 -> django_models_criando_seu_primeiro_model_makemigrations_e_migrate 2025-04-29 23:34:27

--- projeto_agenda_django/contact/models.py
+++ projeto_agenda_django/contact/models.py
@@ -1,3 +1,26 @@
+from django.utils import timezone
 from django.db import models
 
+
 # Create your models here.
+
+# id (primary key - automatico não preocupa)
+# first_name(string) string campo de texto limitado a 255 caracteres
+# last_name(string)
+# phone(string)
+# email(string)
+# created_at(datetime) data de criação do contato
+# description(text)
+# category (foreign key) categoria do contato
+# show (boolean) se o contato deve ser exibido ou não
+# owner (foreign key) usuário dono do contato
+# picture (image) imagem do contato
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(max_length=255)
+    created_date = models.DateField(default=timezone.now)
+    description = models.TextField(blank=True)
