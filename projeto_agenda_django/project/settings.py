a062b6ff531a8ca686e8c3eae6db658e696b918a
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-05-04 18:20:30 -0300

    aula_13 -> script_python_que_gera_dados_aleatórios_no_django_com_o_faker 2025-05-04 18:20:20

--- projeto_agenda_django/project/settings.py
+++ projeto_agenda_django/project/settings.py
@@ -1,3 +1,4 @@
+# flake8:noqa
 """
 Django settings for project project.
 
@@ -14,7 +15,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
